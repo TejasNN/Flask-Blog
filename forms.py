86726976e93e1ce188f97088a8518597a3fb1fb6
TejasNN <tejasnagvekar90@gmail.com>
2025-03-24 00:30:21 +0530

    Updated bootstrap version and added login and register webpages

--- forms.py
+++ forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 class RegistrationForm(FlaskForm):
@@ -9,6 +9,12 @@ class RegistrationForm(FlaskForm):
     
     password = PasswordField('Password', validators=[DataRequired()])
     
-    confirm_password = PasswordField('Confirm Password', validators= [DataRequired(), EqualTo('Password')])
+    confirm_password = PasswordField('Confirm Password', validators= [DataRequired(), EqualTo('password')])
     
+    submit = SubmitField('Sign Up')
     
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Sign Up')
