2f557fdf5b5384af91ef828e63cd2cc3dd7f1bc5
TejasNN <tejasnagvekar90@gmail.com>
2025-03-27 23:31:07 +0530

    Add login and logout functionality based on user session

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.app_context().push()
@@ -8,4 +9,5 @@ app.config['SECRET_KEY'] = '35ca228704b446f3b028e8e2f65aa09c'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
 from flaskblog import routes
