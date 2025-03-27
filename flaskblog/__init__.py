0428e4466d48f46a7a7b51c7faaf419e01ce44ae
TejasNN <tejasnagvekar90@gmail.com>
2025-03-27 18:08:45 +0530

    Add hash to password in register form and add user data to db

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
+app.app_context().push()
 app.config['SECRET_KEY'] = '35ca228704b446f3b028e8e2f65aa09c'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
-
+bcrypt = Bcrypt(app)
 from flaskblog import routes
