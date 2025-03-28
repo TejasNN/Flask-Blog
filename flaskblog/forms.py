8d79dbbc53631cc2dcd722110e1885fa57067122
TejasNN <tejasnagvekar90@gmail.com>
2025-03-28 19:18:13 +0530

    Add account info update functionality

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,4 +1,5 @@
 from flask_wtf import FlaskForm
+from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flaskblog.models import User
@@ -37,16 +38,16 @@ class UpdateAccountForm(FlaskForm):
     
     email = StringField('Email', validators=[DataRequired(), Email()])
     
-    submit = SubmitField('Sign Up')
+    submit = SubmitField('Update')
 
     def validate_username(self, username):
-
-        user = User.query.filter_by(username=username.data).first()
-        if user:
-            raise ValidationError('That username is taken. Please choose another one.')
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('That username is taken. Please choose another one.')
     
     def validate_email(self, email):
-
-        user = User.query.filter_by(email=email.data).first()
-        if user:
-            raise ValidationError('That email is taken. Please choose another one.')
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError('That email is taken. Please choose another one.')
