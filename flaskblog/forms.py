2c82277444b075bb73556cfef707c089088dafa0
TejasNN <tejasnagvekar90@gmail.com>
2025-03-28 15:03:41 +0530

    Add account form and profile picture functionality

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -31,3 +31,22 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
+    
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    
+    submit = SubmitField('Sign Up')
+
+    def validate_username(self, username):
+
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('That username is taken. Please choose another one.')
+    
+    def validate_email(self, email):
+
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('That email is taken. Please choose another one.')
