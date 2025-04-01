fcec1d2775ecf0d232bc8b4be404c9cd86061d7d
TejasNN <tejasnagvekar90@gmail.com>
2025-04-01 18:51:57 +0530

    Add password reset routes html views and mail configure

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -56,3 +56,20 @@ class PostForm(FlaskForm):
     title = StringField('Title', validators=[DataRequired()])
     content = TextAreaField('Content', validators=[DataRequired()])
     submit = SubmitField('Post')
+
+
+class RequestResetForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    submit = SubmitField('Send Password Reset link')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is None:
+            raise ValidationError('There is no account with that email. You must register first')
+        
+
+class ResetPasswordForm(FlaskForm):
+    password = PasswordField('New Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', validators= [DataRequired(), EqualTo('password')])
+    
+    submit = SubmitField('Reset Password')
